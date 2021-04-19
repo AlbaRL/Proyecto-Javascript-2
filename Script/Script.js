5314a0fcb07113ea315206a597ce76fae68c8445
Fermarlin <fermarlinxd@gmail.com>
2021-04-19 13:42:07 +0200

    Creada la rama Dev

--- Script/Script.js
+++ Script/Script.js
@@ -1,9 +1,26 @@
 //INICIALIZACION
 $("#Tangrama").hide();
+let anguloPieza_1=0;
+
 
 $("#start").click(function(){
     $("#Inicio").hide();
     $("#Tangrama").show();
     $( "#Pieza_1" ).draggable();
 
+  });
+
+  $("#Pieza_1").rotate({bind:{
+    click: function(){
+      $(this).rotate({
+        angle: anguloPieza_1,
+        animateTo: anguloPieza_1+90,
+        })
+        anguloPieza_1=anguloPieza_1+90;
+        if(anguloPieza_1==360){
+          anguloPieza_1=0;
+        }
+        console.log(anguloPieza_1);
+      }
+    }
   });
